c99616a9dac6ffa6c16ec0ba77251726dd5b15c7
Mike Mole <michael.mole@lookout.com>
2015-04-05 20:57:14 -0400

    Exercise 4.6

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -9,11 +9,27 @@ sealed trait Either[+E,+A] {
    case Right(a) => Right(f(a))
  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+   case Left(e) => Left(e)
+   case Right(a) => f(a)
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case Left(_) => b
+   case Right(a) => Right(a)
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+// def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = 
+//   (this,b) match {
+//   case (Right(a),Right(b)) => Right(f(a,b))
+//   case (Left(e),_) => Left(e)
+//   case (_, Left(e)) => Left(e)
+// }
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = 
+    for {
+      a <- this
+      b1 <- b
+    } yield f(a, b1)
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
