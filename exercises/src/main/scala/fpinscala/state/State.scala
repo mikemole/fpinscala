4a6ce2a34172e774763802a152fb1eefdf558bbb
Mike Mole <michael.mole@lookout.com>
2015-09-21 09:24:34 -0400

    State and Gen stuff...

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -20,15 +20,67 @@ object RNG {
   type Rand[+A] = RNG => (A, RNG)
 
   val int: Rand[Int] = _.nextInt
+  
+  def boolean(rng: RNG): (Boolean, RNG) =
+    rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
 
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
+  // ACM Q - Always seem to have trouble with pulling variables
+  // out of thin air with scala.  How exactly is this to be read?
+  // Maybe the way to think about it is that you're defining a function that will
+  // get that object passed to it when used.  You don't have it yet, but when the 
+  // function is used, it'll be available then, so you just need to define what'll
+  // happen when you get it.
+  // I need an "a ha" moment with pushing these parameters through
+  // my type definitions and transformations.
+  // Also, I think this signature initially confuses me as I'm thinking
+  // it's a class method, not an object method... I'm thinking, why am
+  // I passing s?
   def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
+                                       // <--- Here we're generally implementing map to return a Rand (i.e. RNG => (B, RNG))
+    rng => {                           // <--- Here we're saying it's mapping a rng (where does rng come from) to...
+      val (a, rng2) = s(rng)           // <--- Here we get to use that rng that we specified on the left by passing to our Rand[A]
+      (f(a), rng2)                     // <--- and returning a tuple, applying our function to "a", with the next state
     }
+    // One more pass at trying to state what's happening...
+    // You start with an s: Rand[A] which is a function that takes an instance of type RNG and returns a tuple
+    // representing a randomly generated A and the RNG's next state.
+    // You also get a function which takes an instance of type A and returns an instance of type B.
+    // Your goal is to re-use the Rand[A] function, but map the As to Bs at the each step of the way.
+    // In other words, please return a function with takes an instance of type RNG and returns a tuple
+    // of (B, RNG) where RNG is the next state.
+    // So, we say, ok, it's going to be a function that is provided an RNG instance, "rng";
+    // we'll have that shit. Given that we have that shit, we're going to take it and return a tuple.
+    // We can't do it in one step, so we'll write an inline function which does some work, but ultimately
+    // returns what we want.  Again, given that we've got that "rng", here's the function that should
+    // be performed with that rng...
+    // That function is going to be to use that "rng" in the s: Rand[A] which we already know
+    // takes a RNG to an (A,RNG).  So we'll use the "rng" there and get our A and our next state "rng2".
+    // Then we'll apply the function, f, to a, and return the tuple.
+    // It might be helpful to think of Rand like you think of "f", just another function, which is entirely accurate.
+    // 
+    // Ok... That's all good I suppose... But now why do we curry the f rather than pass it as a second parameter?
+    // That still always bugs me...
+    // Let's try it..
+  def mapNoCurry[A,B](s: Rand[A], f: A => B): Rand[B] =
+    rng => {
+      val (a,rng2) = s(rng)
+      (f(a),rng2)
+    }
+  // Ah ha, the answer is that without currying I'm not able to define
+  // my "f" in terms of what comes back from my "s" above which is
+  // much more convenient. Instead, I have to define another function
+  // explicitly since it's completely independent.
+  // I could be missing the point...
+  def doubleViaMapNoCurry: Rand[Double] = {
+    def f(i: Int): Double = i / (Int.MaxValue.toDouble + 1)
+    mapNoCurry(nonNegativeInt, f)
+  }
+  
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
 
   // Question... Isn't the book's answer increasing the probability of 0?
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
@@ -43,9 +95,6 @@ object RNG {
     val d = (i / (Int.MaxValue.toDouble + 1))
     (d, r)
   }
-  
-  def doubleViaMap: Rand[Double] =
-    map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
     val (i, r1) = rng.nextInt
@@ -74,12 +123,12 @@ object RNG {
       (List.empty, rng)
     }
   }
-
+  
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rng => {
-      val (a, rng2) = ra(rng)
-      val (b, rng3) = rb(rng2)
-      (f(a,b), rng3)
+      val (a,rng2) = ra(rng)
+      val (b,rng3) = rb(rng2)
+      (f(a,b),rng3)
     }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = 
@@ -91,9 +140,27 @@ object RNG {
   def intsViaSequence(count: Int): Rand[List[Int]] =
     sequence(List.fill(count)(int))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  // Always kind of feel like flatMap should be called mapFlat
+  // because it helps me visualize what's really happening,
+  // but I suppose flatMap is more in line with English conventions
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng =>
+      val (a,rnga) = f(rng)
+      g(a)(rnga)
+  }
+  
+  // This is really something worth analyzing further...
+  // How this combinator lets us avoid handling
+  // the RNG
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
 }
 
+// Having function types with noun names confuses me when using them.
+// Explanation of case class here....
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     sys.error("todo")
@@ -112,4 +179,13 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S,A]], acc: List[A]): (List[A],S) =
+      actions match {
+        case Nil => (acc.reverse,s)
+        case h :: t => h.run(s) match { case (a,s2) => go(s2, t, a :: acc) }
+      }
+    State((s: S) => go(s,sas,List()))
+  }
 }
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -20,14 +20,39 @@ object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
-object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+case class Gen[A](sample: State[RNG,A]) {
+  
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+  
+  /* A method alias for the function we wrote earlier. */
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+        
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size flatMap (n => listOfN(n))
+  
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap { x => if (x) g1 else g2 }
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+object Gen {
+  def unit[A](a: => A): Gen[A] = Gen(State(RNG.unit(a)))
+  
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.boolean))
+    
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+  
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map { x => start + x % (stopExclusive - start) })
 }
+
+//trait Gen[A] {
+//  def map[A,B](f: A => B): Gen[B] = ???
+//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+//}
 
 trait SGen[+A] {
 
